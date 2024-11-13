32e4b93940805435561ceb259c1719098c0f69c3
Nesan Selvan <88973192+NesanSelvan@users.noreply.github.com>
2024-11-13 18:04:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+
+    return {"hello world": "hello"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
